69bbcc69976e519f5da4ff91e36ce180b5b710a0
yii2trial <44015738+yii2trial@users.noreply.github.com>
2019-05-20 13:40:50 +0300

    Added isActive method

--- common/entities/User.php
+++ common/entities/User.php
@@ -221,4 +221,9 @@ class User extends ActiveRecord implements IdentityInterface
     {
         $this->password_reset_token = null;
     }
+
+    public function isActive(): bool
+    {
+        return $this->status === self::STATUS_ACTIVE;
+    }
 }
