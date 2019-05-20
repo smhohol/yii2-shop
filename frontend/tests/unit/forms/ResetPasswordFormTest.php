be72333d207bfa947c903132aab184b65df81348
yii2trial <44015738+yii2trial@users.noreply.github.com>
2019-05-20 16:01:43 +0300

    Do some tests fix after adding password reset service

--- frontend/tests/unit/forms/ResetPasswordFormTest.php
+++ frontend/tests/unit/forms/ResetPasswordFormTest.php
@@ -23,22 +23,12 @@ class ResetPasswordFormTest extends \Codeception\Test\Unit
         ]);
     }
 
-    public function testResetWrongToken()
-    {
-        $this->tester->expectException('\yii\base\InvalidArgumentException', function() {
-            new ResetPasswordForm('');
-        });
-
-        $this->tester->expectException('\yii\base\InvalidArgumentException', function() {
-            new ResetPasswordForm('notexistingtoken_1391882543');
-        });
-    }
-
-    public function testResetCorrectToken()
+    public function testCorrectToken()
     {
         $user = $this->tester->grabFixture('user', 0);
         $form = new ResetPasswordForm($user['password_reset_token']);
-        expect_that($form->resetPassword());
+        $form->password = 'new-password';
+        expect_that($form->validate());
     }
 
 }
