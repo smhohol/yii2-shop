c83f7b986da5d7bcd59965324cd40a971bcdc3d3
yii2trial <44015738+yii2trial@users.noreply.github.com>
2019-05-25 16:24:55 +0300

    Rename user table to users

--- shop/entities/user/User.php
+++ shop/entities/user/User.php
@@ -66,7 +66,7 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function tableName()
     {
-        return '{{%user}}';
+        return '{{%users}}';
     }
 
     /**
