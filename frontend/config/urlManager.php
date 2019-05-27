58d4fa28ee7648249c7c4dcd5c8e5e761b3ac1de
yii2trial <44015738+yii2trial@users.noreply.github.com>
2019-05-27 12:56:12 +0300

    Add user cabinet on frontend
    - signup user can do some setting modification in your site account (cabinet)

--- frontend/config/urlManager.php
+++ frontend/config/urlManager.php
@@ -15,6 +15,12 @@ return [
         'reset-password' => 'auth/reset/request',
         'reset-password/confirm' => 'auth/reset/confirm',
 
+        'cabinet' => 'cabinet/default/index',
+        'cabinet/<_c:[\w\-]+>' => 'cabinet/<_c>/index',
+        'cabinet/<_c:[\w\-]+>/<id:\d+>' => 'cabinet/<_c>/view',
+        'cabinet/<_c:[\w\-]+>/<_a:[\w-]+>' => 'cabinet/<_c>/<_a>',
+        'cabinet/<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => 'cabinet/<_c>/<_a>',
+
         '<_c:[\w\-]+>' => '<_c>/index',
         '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
         '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
