ba8de00bdf1c14cd264a215d71ccdf10ac693dfc
yii2trial <44015738+yii2trial@users.noreply.github.com>
2019-05-27 14:47:57 +0300

    Modify search form model
    - inherited not from User, but from Model

--- backend/forms/UserSearch.php
+++ backend/forms/UserSearch.php
@@ -6,43 +6,31 @@ use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use shop\entities\User\User;
 
-/**
- * UserSearch represents the model behind the search form of `shop\entities\User\User`.
- */
-class UserSearch extends User
+class UserSearch extends Model
 {
-    /**
-     * {@inheritdoc}
-     */
+    public $id;
+    public $created_at;
+    public $username;
+    public $email;
+    public $status;
+
+
     public function rules()
     {
         return [
-            [['id', 'status', 'created_at', 'updated_at'], 'integer'],
-            [['username', 'auth_key', 'password_hash', 'password_reset_token', 'email', 'verification_token'], 'safe'],
+            [['id', 'status', 'created_at'], 'integer'],
+            [['username', 'email'], 'safe'],
         ];
     }
 
     /**
-     * {@inheritdoc}
-     */
-    public function scenarios()
-    {
-        // bypass scenarios() implementation in the parent class
-        return Model::scenarios();
-    }
-
-    /**
-     * Creates data provider instance with search query applied
-     *
      * @param array $params
      *
      * @return ActiveDataProvider
      */
-    public function search($params)
+    public function search(array $params): ActiveDataProvider
     {
         $query = User::find();
-
-        // add conditions that should always apply here
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
@@ -51,25 +39,18 @@ class UserSearch extends User
         $this->load($params);
 
         if (!$this->validate()) {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
+            $query->where('0=1');
             return $dataProvider;
         }
 
-        // grid filtering conditions
         $query->andFilterWhere([
             'id' => $this->id,
             'status' => $this->status,
             'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
         ]);
 
         $query->andFilterWhere(['like', 'username', $this->username])
-            ->andFilterWhere(['like', 'auth_key', $this->auth_key])
-            ->andFilterWhere(['like', 'password_hash', $this->password_hash])
-            ->andFilterWhere(['like', 'password_reset_token', $this->password_reset_token])
-            ->andFilterWhere(['like', 'email', $this->email])
-            ->andFilterWhere(['like', 'verification_token', $this->verification_token]);
+            ->andFilterWhere(['like', 'email', $this->email]);
 
         return $dataProvider;
     }
