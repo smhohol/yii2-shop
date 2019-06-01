d1a511fb7c56410f1ae71ef9b3b7a80a3e3757a1
yii2trial <44015738+yii2trial@users.noreply.github.com>
2019-06-01 11:22:29 +0300

    Add tag
    - don't forget to make a migration to TEST database
    vagrant@shop:/app$ ./yii_test migrate

---
+++ console/migrations/m190530_113123_create_tags_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\db\Migration;
+
+class m190530_113123_create_tags_table extends Migration
+{
+    public function up()
+    {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+
+        $this->createTable('{{%shop_tags}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'slug' => $this->string()->notNull(),
+        ], $tableOptions);
+
+        $this->createIndex('{{%idx-shop_tags-slug}}', '{{%shop_tags}}', 'slug', true);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%shop_tags}}');
+    }
+}
